4c03b0f2039ebc7c43fd80a98aa4e536c26b073a
Zimeng Xiong <zimeng.zx@gmail.com>
2025-07-23 13:37:50 -0700

    fix weird looking Y

--- hw.runs/impl_1/runme.sh
+++ hw.runs/impl_1/runme.sh
@@ -21,7 +21,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='/home/zimengx/RaySketchers/basys3IODemo/hw.runs/impl_1'
+HD_PWD='/home/zimengx/RaySketchers/VGADisplayName/hw.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -38,7 +38,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log GPIO_demo.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source GPIO_demo.tcl -notrace
 
 
